25c6c912fb9e123deb2f74eb377b9aa46195e00a
RAZUMOV <sr0876392@otc.edu>
2018-09-05 08:21:58 -0500

    Cut to 10 lines

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,9 @@
-var x = 100;
-for (i = 0; i < x; i++) {
+for (i = 0; i < 100; i++) {
     if (i % 3 == 0)
         console.log ("Fizz");
     else if (i % 5 == 0)
         console.log ("Buzz");
-    if (i % 5 == 0 && i % 3 == 0) // comment out lines 7,8 to get Fizz or Buzz only
+    if (i % 5 == 0 && i % 3 == 0) // comment out lines 7,8 to get Fizz and Buzz only
         console.log ("FizzBuzz");
     else
         console.log(i);
