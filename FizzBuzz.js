7535235a248f567a2fd2b3a378201a0201b7004f
RAZUMOV <sr0876392@otc.edu>
2018-09-05 08:12:30 -0500

    Changed FizzBuzz line

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -4,7 +4,7 @@ for (i = 0; i < x; i++) {
         console.log ("Fizz");
     else if (i % 5 == 0)
         console.log ("Buzz");
-    if (i % 5 == 0 || i % 3 == 0) // comment out lines 7,8 to get Fizz or Buzz only
+    if (i % 5 == 0 && i % 3 == 0) // comment out lines 7,8 to get Fizz or Buzz only
         console.log ("FizzBuzz");
     else
         console.log(i);
